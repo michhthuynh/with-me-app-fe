a6104afe6d358aea4818743a9b81480944cde747
Huynh Hoang <mich.hthuynh@gmail.com>
2020-10-17 12:52:52 +0700

    first commit

---
+++ src/components/TopBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function TopBar(props) {
+    return (
+        <div className="top-bar">
+            <div className="top-bar__left">
+                <ul>
+                    <NavLink to="/category/tv-series">TV Series</NavLink>
+                    <NavLink to="/category/tv-shows">TV Shows</NavLink>
+                    <NavLink to="/category/movies">Movies</NavLink>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TopBar;
