d1b85ecdaf6f8e96c18c2219f2b166a22de04ecf
Huynh Hoang <mich.hthuynh@gmail.com>
2020-10-17 18:05:24 +0700

    setup folder

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TopBar from '../components/TopBar';
+
+Login.propTypes = {
+
+};
+
+function Login(props) {
+    return (
+        <div>
+            <TopBar />
+        </div>
+    );
+}
+
+export default Login;
