d0c37e61b40854a4b8c5548b491b4799c91fb523
Huynh Hoang <mich.hthuynh@gmail.com>
2020-10-17 23:33:47 +0700

    update file

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function LoginForm(props) {
+    return (
+        <div className="login-form">
+            <div className="login-form__left">
+
+            </div>
+            <div className="login-form__main">
+                <div className="login-form__main__title">
+
+                </div>
+                <div className="login-form__main__logo">
+
+                </div>
+                <div className="login-form__main_form">
+                    <div className="form-group">
+                        <label t></label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
